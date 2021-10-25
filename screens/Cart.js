519acd978d776e0b402faf99479ed4182975ce3a
Stephen Asamoah <stephenasamoah515@gmail.com>
2021-10-25 06:40:35 +0000

    added screens

---
+++ screens/Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+
+export default function Cart(){
+    return(
+        <View style={{flex:1}}>
+            <Text>This is the Cart Page</Text>
+        </View>
+    );
+}
---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+
+
+export default function Home(){
+    return(
+        <View style={{flex:1}}>
+            <Text>This is the Home Page</Text>
+        </View>
+    );
+}
